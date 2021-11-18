e86ef24cc5e158d85773eb23906d92f402a6e713
Chen Yu <chenyu.nus@gmail.com>
2021-11-18 12:53:00 +0000

    added foam

---
+++ _layouts/gatsby-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  pathPrefix: `/foam-gatsby-template`,
+  siteMetadata: {
+    title: `Foam`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-theme-garden`,
+      options: {
+        rootNote: "/README",
+        contentPath: `${__dirname}/..`,
+        ignore: [
+          "**/_layouts/**",
+          "**/.git/**",
+          "**/.github/**",
+          "**/.vscode/**",
+        ],
+      },
+    },
+  ],
+};
